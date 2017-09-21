37e2ebfb014d082b938e2e4f49b9d694b06bab4c
xrr2016 <xiaoranran1993@163.com>
2017-09-22 07:08:46 +0800

    :art:

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-console.log('Hello!')
+new Typed('#name', {
+  strings: ['你好......', '我是轻键快码', '很高兴见到你'],
+  typeSpeed: 100,
+  backSpeed: 0,
+  fadeOut: true,
+  showCursor: false,
+  loop: true
+})
